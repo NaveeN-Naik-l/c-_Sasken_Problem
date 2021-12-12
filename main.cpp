695f4e509ecbad0b1752d1550e72b896b91ea817
NaveeN-Naik-l <95856141+NaveeN-Naik-l@users.noreply.github.com>
2021-12-12 02:40:12 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <cstring>
+#include <iomanip>
+ 
+using namespace std;
+ 
+int main() 
+{
+    
+    cout<<"1. Select to convert Celcius to Fahrenheit: "<<endl;
+    cout<<"2. Select to convert Fahrenheit to Celcius: "<<endl;
+    cout<<"3. Select to Exit: "<<endl;
+    cout<<"====================================================="<<endl;
+    
+    int choice;
+    cout<<"Enter your Choice: ";
+    cin>>choice;
+    cout<<"====================================================="<<endl;
+    
+    switch(choice)
+    {
+                        
+                
+    }
+    
+ 
+    return 0;
+}
+ 
